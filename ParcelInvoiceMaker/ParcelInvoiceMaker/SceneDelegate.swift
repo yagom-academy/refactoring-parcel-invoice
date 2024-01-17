96829f6c99a03df578ae09603327838bacecb840
yagom <yagom@yagom.net>
2024-01-17 18:31:20 +0900

    mission project added

---
+++ ParcelInvoiceMaker/ParcelInvoiceMaker/SceneDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  ParcelInvoiceMaker - SceneDelegate.swift
+//  Created by yagom. 
+//  Copyright © yagom. All rights reserved.
+// 
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let scene: UIWindowScene = (scene as? UIWindowScene) else { return }
+        
+        let viewController: ParcelOrderViewController = ParcelOrderViewController()
+        let navigationController: UINavigationController = UINavigationController(rootViewController: viewController)
+        navigationController.navigationBar.prefersLargeTitles = true
+        navigationController.navigationBar.tintColor = .black
+        
+        let window: UIWindow = UIWindow(windowScene: scene)
+        window.rootViewController = navigationController
+        
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+}
+
