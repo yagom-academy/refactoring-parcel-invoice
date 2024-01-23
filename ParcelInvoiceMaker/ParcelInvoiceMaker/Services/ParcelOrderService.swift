6796a92de081b9fadb6c0b6ff557a17956ddf904
Hyosung <hyosung@finda.co.kr>
2024-01-24 00:16:16 +0900

    feature: 놓쳤던 AnyObject 추가

--- ParcelInvoiceMaker/ParcelInvoiceMaker/Services/ParcelOrderService.swift
+++ ParcelInvoiceMaker/ParcelInvoiceMaker/Services/ParcelOrderService.swift
@@ -5,7 +5,7 @@
 //  Created by hyosung on 1/23/24.
 //
 
-protocol ParcelOrderService {
+protocol ParcelOrderService: AnyObject {
   func process(
     parcelInformation: ParcelInformation,
     onComplete: (ParcelInformation) -> Void
