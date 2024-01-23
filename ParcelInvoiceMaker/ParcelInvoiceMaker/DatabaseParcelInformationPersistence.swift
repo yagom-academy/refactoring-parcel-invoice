0a274e3a822ec50b5c788f2b87988b62a5a3d5af
ChangMin <sopg04@gmail.com>
2024-01-23 13:58:43 +0900

    step1 - SOLID SRP 적용

---
+++ ParcelInvoiceMaker/ParcelInvoiceMaker/DatabaseParcelInformationPersistence.swift
@@ -0,0 +1,16 @@
+//
+//  DatabaseParcelInformationPersistence.swift
+//  ParcelInvoiceMaker
+//
+//  Created by ChangMin on 1/23/24.
+//
+
+import Foundation
+
+class DatabaseParcelInformationPersistence {
+    
+    func save(parcelInformation: ParcelInformation) {
+        // 데이터베이스에 주문 정보 저장
+        print("발송 정보를 데이터베이스에 저장했습니다.")
+    }
+}
--- ParcelInvoiceMaker/ParcelInvoiceMaker/ParcelProcessor.swift
+++ ParcelInvoiceMaker/ParcelInvoiceMaker/ParcelProcessor.swift
@@ -7,18 +7,18 @@
 import Foundation
 
 class ParcelOrderProcessor {
+    private let dataBase: DatabaseParcelInformationPersistence
+    
+    init(dataBase: DatabaseParcelInformationPersistence) {
+        self.dataBase = dataBase
+    }
     
     // 택배 주문 처리 로직
     func process(parcelInformation: ParcelInformation, onComplete: (ParcelInformation) -> Void) {
         
         // 데이터베이스에 주문 저장
-        save(parcelInformation: parcelInformation)
+        dataBase.save(parcelInformation: parcelInformation)
         
         onComplete(parcelInformation)
-    }
-    
-    func save(parcelInformation: ParcelInformation) {
-        // 데이터베이스에 주문 정보 저장
-        print("발송 정보를 데이터베이스에 저장했습니다.")
     }
 }
