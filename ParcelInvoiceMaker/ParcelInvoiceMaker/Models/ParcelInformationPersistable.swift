958f5ea2b72969c9e8d298ecfe25c854187f43f7
Beepeach <eh7457303j@gmail.com>
2024-01-26 02:50:38 +0900

    Feat: ParcelInformationPersistable 프로토콜 추가
    ParcelInfomation을 데이터베이스에 저장할 수 있는 역할을 ParcelOrderProcessor에서 분리하기위해서 프로토콜을 추가했습니다.
    구체타입에 의존하지 않도록 하기위해서 프로토콜을 먼저 구현했습니다.

---
+++ ParcelInvoiceMaker/ParcelInvoiceMaker/Models/ParcelInformationPersistable.swift
@@ -0,0 +1,13 @@
+//
+//  ParcelInformationPersistable.swift
+//  ParcelInvoiceMaker
+//
+//  Created by JunHeeJo on 1/26/24.
+//
+
+import Foundation
+
+protocol ParcelInformationPersistable {
+    func save(parcelInformation: ParcelInformation)
+}
+
