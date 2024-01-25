b0f0e78dff6fbd75aaec99546d6f48182db8c12b
Beepeach <eh7457303j@gmail.com>
2024-01-26 02:53:40 +0900

    Refact: ParcelOrderProcessor가 단일 책임을 가지도록 DB에 저장하는 역할을 담당하는 객체 의존성 추가
    ParcelOrderProcessor가 택배 주문을 처리하면서 DB에 저장하는 역할도 담당하고 있어서 의존성을 추가시켜줬습니다.

--- ParcelInvoiceMaker/ParcelInvoiceMaker/Models/ParcelProcessor.swift
+++ ParcelInvoiceMaker/ParcelInvoiceMaker/Models/ParcelProcessor.swift
@@ -21,18 +21,15 @@ enum Discount: Int {
 }
 
 class ParcelOrderProcessor {
+    private let database: ParcelInformationPersistable
+    
+    init(database: ParcelInformationPersistable) {
+        self.database = database
+    }
     
     // 택배 주문 처리 로직
     func process(parcelInformation: ParcelInformation, onComplete: (ParcelInformation) -> Void) {
-        
-        // 데이터베이스에 주문 저장
-        save(parcelInformation: parcelInformation)
-        
+        database.save(parcelInformation: parcelInformation)
         onComplete(parcelInformation)
-    }
-    
-    func save(parcelInformation: ParcelInformation) {
-        // 데이터베이스에 주문 정보 저장
-        print("발송 정보를 데이터베이스에 저장했습니다.")
     }
 }
