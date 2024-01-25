ca94901c7df2b84b1e95bfcb88af2ced28e8e0e5
sapere4ude <eileenyoo2@gmail.com>
2024-01-25 21:24:59 +0900

    코드에 작성된 문자를 상수화하기 및 매직넘버 정리

---
+++ ParcelInvoiceMaker/ParcelInvoiceMaker/Enum.swift
@@ -0,0 +1,21 @@
+//
+//  Enum.swift
+//  ParcelInvoiceMaker
+//
+//  Created by Kant on 1/25/24.
+//
+
+import Foundation
+
+enum UI {
+    static let name: String = "이름"
+    static let mobile: String = "전화"
+    static let address: String = "주소"
+    static let cost: String = "요금"
+    static let discount: String = "할인"
+    static let notification: String = "알림"
+    static let sendParcel: String = "택배 보내기"
+    static let none: String = "없음"
+    static let vip: String = "VIP"
+    static let coupon: String = "쿠폰"
+}
