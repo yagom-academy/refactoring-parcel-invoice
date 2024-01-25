a150a15abe53ad23c704a0b9652b20770d184a4e
Beepeach <eh7457303j@gmail.com>
2024-01-26 02:37:27 +0900

    Refact: Name 구조체에 있는 매직 넘버 제거
    가, 힣, 2, 8과 같은 말을 이해할 수 없는 경우가 있으므로 따로 분리해줬습니다.

--- ParcelInvoiceMaker/ParcelInvoiceMaker/Models/Name.swift
+++ ParcelInvoiceMaker/ParcelInvoiceMaker/Models/Name.swift
@@ -11,8 +11,15 @@ struct Name {
     let value: String
     
     init(_ value: String) throws {
-        let koreanNameRegex: String = "^[가-힣]{2,8}$"
+        let koreanNameRegex: String = "^[\(KoreanNameRules.firstKoreanUnicode)-\(KoreanNameRules.lastKoreanUnicode)]{\(KoreanNameRules.minNameCount),\(KoreanNameRules.maxNameCount)}$"
         guard  value.range(of: koreanNameRegex, options: .regularExpression) != nil else { throw NSError() }
         self.value = value
     }
+}
+
+enum KoreanNameRules {
+    static let firstKoreanUnicode: String = "가"
+    static let lastKoreanUnicode: String = "힣"
+    static let minNameCount: Int = 2
+    static let maxNameCount: Int = 8
 }
