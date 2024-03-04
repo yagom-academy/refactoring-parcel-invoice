74f047da18f2fd2284d84cece4956d61984de748
OliverChoi-iOS <cjk921211@gmail.com>
2024-03-04 20:16:21 +0900

    STEP1: SOLID SRP 적용
    1. DatabaseParcelInformationPersistence struct type을 만들어 데이터베이스에 저장하는 기능(save)을 분리
    2. ParcelOrderProcessor 내의 불필요한 save function을 제거하고, process 중에 프로퍼티로 분리한 DatabaseParcelInformationPersistence 의 save 사용

--- ParcelInvoiceMaker/ParcelInvoiceMaker/Feature/Parcel/ParcelProcessor.swift
+++ ParcelInvoiceMaker/ParcelInvoiceMaker/Feature/Parcel/ParcelProcessor.swift
@@ -6,19 +6,23 @@
 
 import Foundation
 
-class ParcelOrderProcessor {
+struct DatabaseParcelInformationPersistence {
+    func save(parcelInformation: ParcelInformation) {
+        // 데이터베이스에 주문 정보 저장
+        print("발송 정보를 데이터베이스에 저장했습니다.")
+    }
+}
+
+final class ParcelOrderProcessor {
+    
+    private let databaseParcelInformationPersistence: DatabaseParcelInformationPersistence = .init()
     
     // 택배 주문 처리 로직
     func process(parcelInformation: ParcelInformation, onComplete: (ParcelInformation) -> Void) {
         
         // 데이터베이스에 주문 저장
-        save(parcelInformation: parcelInformation)
+        databaseParcelInformationPersistence.save(parcelInformation: parcelInformation)
         
         onComplete(parcelInformation)
-    }
-    
-    func save(parcelInformation: ParcelInformation) {
-        // 데이터베이스에 주문 정보 저장
-        print("발송 정보를 데이터베이스에 저장했습니다.")
     }
 }
