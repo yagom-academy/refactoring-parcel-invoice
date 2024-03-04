2b50d458f35cfb3dbb402a13a5de9f03f7fb5778
hsw1920 <hsw1920@gmail.com>
2024-03-04 21:26:18 +0900

    STEP1: DatabaseParcelInformationPersistence 파일 분리

--- ParcelInvoiceMaker/ParcelInvoiceMaker/Source/Service/ParcelProcessor.swift
+++ ParcelInvoiceMaker/ParcelInvoiceMaker/Source/Service/ParcelProcessor.swift
@@ -6,17 +6,6 @@
 
 import Foundation
 
-protocol ParcelInformationPersistence {
-    func save(parcelInformation: ParcelInformation)
-}
-
-final class DatabaseParcelInformationPersistence: ParcelInformationPersistence {
-    func save(parcelInformation: ParcelInformation) {
-        // 데이터베이스에 주문 정보 저장
-        print("발송 정보를 데이터베이스에 저장했습니다.")
-    }
-}
-
 protocol ParcelOrderProcessable {
     func process(parcelInformation: ParcelInformation,
                  onComplete: (ParcelInformation) -> Void)
