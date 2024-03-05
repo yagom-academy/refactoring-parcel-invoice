bee240e7dca12d9cd2499e07faa511ffafe04659
sm-amoled <moktak072@naver.com>
2024-03-05 17:02:32 +0900

    refactor: Processor 에 DIP 적용

--- ParcelInvoiceMaker/ParcelInvoiceMaker/ParcelProcessor.swift
+++ ParcelInvoiceMaker/ParcelInvoiceMaker/ParcelProcessor.swift
@@ -21,17 +21,26 @@ enum Discount: Int {
 }
 
 class ParcelOrderProcessor {
-    let databasePersistence: DatabaseParcelInformationPersistence = DatabaseParcelInformationPersistence();
+    
+    init(parcelInformationPersistence: ParcelInformationPersistence) {
+        self.parcelInformationPersistence = parcelInformationPersistence
+    }
+    
+    let parcelInformationPersistence: ParcelInformationPersistence
     // 택배 주문 처리 로직
     func process(parcelInformation: ParcelInformation, onComplete: (ParcelInformation) -> Void) {
         
         // 데이터베이스에 주문 저장
-        databasePersistence.save(parcelInformation: parcelInformation)
+        parcelInformationPersistence.save(parcelInformation: parcelInformation)
         onComplete(parcelInformation)
     }
 }
 
-class DatabaseParcelInformationPersistence {
+protocol ParcelInformationPersistence {
+    func save(parcelInformation: ParcelInformation)
+}
+
+class DatabaseParcelInformationPersistence: ParcelInformationPersistence {
     func save(parcelInformation: ParcelInformation) {
         // 데이터베이스에 주문 정보 저장
         print("발송 정보를 데이터베이스에 저장했습니다.")
