8acf691f53b163c814d072c98be37cb7c3b0f919
hsw1920 <hsw1920@gmail.com>
2024-03-05 17:44:40 +0900

    STEP2: Constant 추가

---
+++ ParcelInvoiceMaker/ParcelInvoiceMaker/Source/Model/Discount.swift
@@ -0,0 +1,8 @@
+//
+//  DiscountPolicy.swift
+//  ParcelInvoiceMaker
+//
+//  Created by 홍승완 on 2024/03/05.
+//
+
+import Foundation
---
+++ ParcelInvoiceMaker/ParcelInvoiceMaker/Source/Model/DiscountStrategy.swift
@@ -0,0 +1,8 @@
+//
+//  DiscountStrategy.swift
+//  ParcelInvoiceMaker
+//
+//  Created by 홍승완 on 2024/03/05.
+//
+
+import Foundation
