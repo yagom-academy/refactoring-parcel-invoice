d5fdcb319d7056f19e0ff46de6210e49ffac6b2e
OliverChoi-iOS <cjk921211@gmail.com>
2024-03-05 09:58:45 +0900

    fix: 패키지 분류 오류 수정

--- ParcelInvoiceMaker/ParcelInvoiceMaker/Processor/Parcel/ParcelProcessor.swift
+++ ParcelInvoiceMaker/ParcelInvoiceMaker/Processor/Parcel/ParcelProcessor.swift
@@ -6,19 +6,6 @@
 
 import Foundation
 
-protocol ParcelInformationPersistence {
-    
-    func save(parcelInformation: ParcelInformation)
-}
-
-struct DatabaseParcelInformationPersistence: ParcelInformationPersistence {
-    
-    func save(parcelInformation: ParcelInformation) {
-        // 데이터베이스에 주문 정보 저장
-        print("발송 정보를 데이터베이스에 저장했습니다.")
-    }
-}
-
 protocol ParcelOrderProcessor {
     
     func process(parcelInformation: ParcelInformation, onComplete: (ParcelInformation) -> Void)
