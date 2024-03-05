38b4d39bf4af6d9d85d6fbef8e1e04c532d7bc01
OliverChoi-iOS <cjk921211@gmail.com>
2024-03-05 09:54:23 +0900

    Revert "STEP2: SOLID OCP / 객체 미용 체조 2원칙 적용"
    This reverts commit b0d9be1507fb2d0b63b193d5a9dd38dfe080f0f0.

--- ParcelInvoiceMaker/ParcelInvoiceMaker/Feature/Parcel/Model/ParcelCostInfo.swift
+++ ParcelInvoiceMaker/ParcelInvoiceMaker/Feature/Parcel/Model/ParcelCostInfo.swift
@@ -31,40 +31,8 @@ struct ParcelDeliveryCost: ValidatedNumeric {
     }
 }
 
-protocol DiscountStrategy {
-    func applyDiscount(deliveryCost: Int) -> Int
-}
-
-struct NoDiscount: DiscountStrategy {
-    func applyDiscount(deliveryCost: Int) -> Int {
-        deliveryCost
-    }
-}
-
-struct VIPDiscount: DiscountStrategy {
-    func applyDiscount(deliveryCost: Int) -> Int {
-        deliveryCost / 5 * 4
-    }
-}
-
-struct CouponDiscount: DiscountStrategy {
-    func applyDiscount(deliveryCost: Int) -> Int {
-        deliveryCost / 2
-    }
-}
-
 enum Discount: Int {
     case none = 0, vip, coupon
-    var strategy: DiscountStrategy {
-        switch self {
-        case .none:
-            NoDiscount()
-        case .vip:
-            VIPDiscount()
-        case .coupon:
-            CouponDiscount()
-        }
-    }
 }
 
 struct ParcelCostInfo {
