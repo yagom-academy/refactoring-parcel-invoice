5abffd491b8eed43c0249d70e4cd16f5bfe894e9
Qussk <by1106007@gmail.com>
2024-03-07 09:07:56 +0300

    feat: SOLID SRP 적용

--- ParcelInvoiceMaker/ParcelInvoiceMaker/ParcelOrderViewController.swift
+++ ParcelInvoiceMaker/ParcelInvoiceMaker/ParcelOrderViewController.swift
@@ -6,9 +6,9 @@
 
 import UIKit
 
-class ParcelOrderViewController: UIViewController, ParcelOrderProcessorProcessProtocol & ParcelOrderProcessorSaveProtocol , ParcelOrderViewDelegate {
+class ParcelOrderViewController: UIViewController, ParcelOrderViewDelegate {
     
-//    private let parcelProcessor: ParcelOrderProcessor = ParcelOrderProcessor()
+    private let parcelProcessor: ParcelOrderProcessor = ParcelOrderProcessor()
     
     init() {
         super.init(nibName: nil, bundle: nil)
@@ -20,28 +20,13 @@ class ParcelOrderViewController: UIViewController, ParcelOrderProcessorProcessPr
     }
     
     func parcelOrderMade(_ parcelInformation: ParcelInformation) {
-        process(parcelInformation: parcelInformation) { (parcelInformation) in
+        parcelProcessor.process(parcelInformation: parcelInformation) { (parcelInformation) in
             let invoiceViewController: InvoiceViewController = .init(parcelInformation: parcelInformation)
             navigationController?.pushViewController(invoiceViewController, animated: true)
         }
         
     }
     
-    func process(parcelInformation: ParcelInformation, onComplete: (ParcelInformation) -> Void) {
-        
-        // 데이터베이스에 주문 저장
-        save(parcelInformation: parcelInformation)
-        
-        onComplete(parcelInformation)
-    }
-
-    
-    func save(parcelInformation: ParcelInformation) {
-        // 데이터베이스에 주문 정보 저장
-        print("발송 정보를 데이터베이스에 저장했습니다.")
-
-    }
-
 
     override func loadView() {
         view = ParcelOrderView(delegate: self)
