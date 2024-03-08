d5f004dfcf6a951d39b9c86ab388a8f3022e88e9
samlee.develop <samlee.develop>
2024-03-08 18:14:40 +0900

    SOLID SRP 적용 : 저장기능 분리

--- ParcelInvoiceMaker/ParcelInvoiceMaker/ParcelProcessor.swift
+++ ParcelInvoiceMaker/ParcelInvoiceMaker/ParcelProcessor.swift
@@ -70,15 +70,19 @@ class CostInfo {
 
 class ParcelOrderProcessor {
     
+    private let databaseParcelInformationPersistence: DatabaseParcelInformationPersistence = DatabaseParcelInformationPersistence()
+    
     // 택배 주문 처리 로직
     func process(parcelInformation: ParcelInformation, onComplete: (ParcelInformation) -> Void) {
         
         // 데이터베이스에 주문 저장
-        save(parcelInformation: parcelInformation)
+        databaseParcelInformationPersistence.save(parcelInformation: parcelInformation)
         
         onComplete(parcelInformation)
     }
-    
+}
+
+fileprivate final class DatabaseParcelInformationPersistence {
     func save(parcelInformation: ParcelInformation) {
         // 데이터베이스에 주문 정보 저장
         print("발송 정보를 데이터베이스에 저장했습니다.")
